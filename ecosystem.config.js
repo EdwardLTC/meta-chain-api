769517f7c085e0c71aacb935e38678a0d369be23
Edward <lethanhcong06062003@gmail.com>
2025-11-24 15:39:53 +0700

    Add PM2 configuration for process management and update package scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,17 +6,8 @@ module.exports = {
       instances: 1,
       exec_mode: 'fork',
       autorestart: true,
-      watch: false,
-      max_restarts: 10,
-      restart_delay: 5000,
       output: './logs/meta-chain-api/out.log',
       error: './logs/meta-chain-api/error.log',
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
     },
   ],
 };
